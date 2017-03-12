78415d4e17d124b0cafcb39209f64835f132fab1
msyfls123 <m18771058712@live.com>
2017-03-12 17:50:33 +0800

    make cugthesis alpha

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # this is for Unix/Linux scripts
 sh clean.sh
+# xelatex main
+# bibtex main
 xelatex main
-bibtex main
 xelatex main
-xelatex main
-xelatex cover
+# xelatex cover
